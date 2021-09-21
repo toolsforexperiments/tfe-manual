f8d598ce0e0f562f1fd075edeb3197368e6f3588
pfafflabatuiuc <72898175+pfafflabatuiuc@users.noreply.github.com>
2021-09-21 15:57:03 -0500

    third try on the config files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../instrumentserver'))
 sys.path.insert(0, os.path.abspath('../../instrumentserver/instrumentserver/'))
+sys.path.insert(0, os.path.abspath('../../instrumentserver/instrumentserver/helpers.py'))
 
 
 # -- Project information -----------------------------------------------------
